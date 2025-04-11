93f0a2839135d0878b83ffa7b2495e4b9533c12a
Vitor Dornela <vitor.dornela.m@gmail.com>
2025-04-10 21:31:44 -0300

    10-04-25

---
+++ src/main/java/br/faesa/aula3_lista_encadeada/TesteLSE.java
@@ -0,0 +1,19 @@
+package br.faesa.aula3_lista_encadeada;
+
+import br.faesa.aula2_lista_contigua.Item;
+
+public class TesteLSE {
+
+	public static void main(String[] args) {
+		LSEItem lista = new LSEItem();
+		
+		lista.insereInicio(new Item(12));
+		lista.insereInicio(new Item(55));
+		lista.insereInicio(new Item(37));
+		lista.insereInicio(new Item(22));
+		
+		
+
+	}
+
+}
