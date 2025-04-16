ad473f9afeea92223ef8d8bd4683c0235867e9db
Vitor Dornela <vitor.dornela.m@gmail.com>
2025-04-16 19:34:53 -0300

    16-04-25

--- src/main/java/br/faesa/aula3_lista_encadeada/LSEItem.java
+++ src/main/java/br/faesa/aula3_lista_encadeada/LSEItem.java
@@ -3,77 +3,79 @@ package br.faesa.aula3_lista_encadeada;
 import br.faesa.aula2_lista_contigua.Item;
 
 public class LSEItem {
-	private NoItem prim, ult;
-	private int quant;
-	
-	public LSEItem() {
-		this.prim = null;
-		this.ult = null;
-		this.quant = 0;
-	}
-	
-	public int tamanho() {
-		return this.quant;
-	}
-	
-	public boolean eVazia() {
-		return (quant==0); // true se = 0, false se != 0
-	}
-	
-	public NoItem get (int pos) {
-		NoItem aux;
-		// verificar se a posicao e valida
-		if (pos>=0 && pos<quant) {
-		if (pos==quant-1) {
-			return this.ult;
-		}
-		aux = this.prim;
-		for (int i=0; i<pos; i++) {
-			aux = aux.getProx();
-		}
-		return aux;
-	}
-		return null; // se a posicao for invalida
-	}
-	
-	public NoItem pesquisa (int cod) {
-		NoItem aux;
-		aux = this.prim;
-		while (aux!=null) {
-			if (aux.getItem().getCodigo()==cod) {
-				return aux;
-			}
-			aux = aux.getProx();
-		}
-		return aux;
-	}
-	
-	public void insereInicio (Item item) {
-		NoItem no = new NoItem(item);
-		
-		if (eVazia()) {
-			this.prim = no;
-			this.ult = no;
-		} else {
-			no.setProx(this.prim);
-			this.prim = no;
-		}
-		this.quant++;
-	}
-	
-	public String toString() {
-		String temp="";
-		NoItem aux = this.prim;
-		
-		while (aux != null) {
-			temp += aux.getItem()+"\n";
-			aux = aux.getProx();
-		}
-		return temp;
-	}
+    private NoItem prim, ult;
+    private int quant;
+
+    public LSEItem() {
+        this.prim = null;
+        this.ult = null;
+        this.quant = 0;
+    }
+
+    public int tamanho() {
+        return this.quant;
+    }
+
+    public boolean eVazia() {
+        return (quant == 0); // true se = 0, false se != 0
+    }
+
+    public NoItem get(int pos) {
+        NoItem aux;
+        // verificar se a posicao e valida
+        if (pos >= 0 && pos < quant) {
+            if (pos == quant - 1) {
+                return this.ult;
+            }
+            aux = this.prim;
+            for (int i = 0; i < pos; i++) {
+                aux = aux.getProx();
+            }
+            return aux;
+        }
+        return null; // se a posicao for invalida
+    }
+
+    public NoItem pesquisa(int cod) {
+        NoItem aux;
+        aux = this.prim;
+        while (aux != null) {
+            if (aux.getItem().getCodigo() == cod) {
+                return aux;
+            }
+            aux = aux.getProx();
+        }
+        return aux;
+    }
+
+    public void insereInicio(Item item) {
+        NoItem no = new NoItem(item);
+
+        if (eVazia()) {
+            this.prim = no;
+            this.ult = no;
+        } else {
+            no.setProx(this.prim);
+            this.prim = no;
+        }
+        this.quant++;
+    }
+
+    public String toString() {
+        String temp = "";
+        NoItem aux = this.prim;
+
+        while (aux != null) {
+            temp += aux.getItem() + "\n";
+            aux = aux.getProx();
+        }
+        return temp;
+    }
 	/*
 	
 	public boolean insere (Integer item, int pos) {
+
+	
 
 	}
 	
---
+++ src/main/java/br/faesa/avaliacaoC1/Questao4.java
@@ -0,0 +1,46 @@
+package br.faesa.avaliacaoC1;
+import br.faesa.aula2_lista_contigua.Item;
+import br.faesa.aula2_lista_contigua.LCInteiro;
+
+import java.util.Scanner;
+
+public class Questao4 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+/*
+    a) Preencha uma lista da classe LCInteiro (deve ser um método_)
+    b) Crie uma segunda lista que receberá o dobro de cada elemento da primeira lista (deve ser um método_). Por exemplo:
+        lista1 = 1, 3, 6
+        lista2 = 2, 6, 12
+    c) Tire a média dos elementos da lista1 e depois lista2 (faça por métodos)
+    d) Mostre na tela as duas listas e o resultado de cada média
+ */
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void preencher(LCInteiro lista1) {
+        int tamanho;
+        System.out.println("Tamanho do lista: ");
+        tamanho = scanner.nextInt();
+        int aux;
+        for (int i = 0; i < tamanho; i++) {
+            aux = (int) (Math.random() * 30 + 1);
+            lista1.insereFinal(aux);
+        }
+    }
+
+    private static void formar(LCInteiro lista2, LCInteiro lista1) {
+        int aux;
+        for (int i = 0; i < lista1.getQuant(); i++) {
+            aux = lista1.get(i) * 2;
+            lista2.insereFinal(aux);
+        }
+    }
+
+    private static double media(LCInteiro lista2) {
+
+        return 0;
+    }
+}
