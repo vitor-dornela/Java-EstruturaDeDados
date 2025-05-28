b2c6ff0f62cd074012130183d2cdb48a2340a19b
Vitor Dornela <vitor.dornela.m@gmail.com>
2025-05-28 20:02:35 -0300

    28-05-25

---
+++ src/main/java/br/faesa/aula8_arvore_binaria_busca/ABBInt.java
@@ -0,0 +1,23 @@
+package br.faesa.aula8_arvore_binaria_busca;
+
+import br.faesa.entidades.NoArv;
+
+public class ABBInt {
+    /*
+    operações
+    1 - construtor
+    2 - pesquisa
+    3 - inserir
+    4 - remover
+    5 - como caminhar
+    6 - toString
+     */
+    private NoArv raiz;
+    private int quant;
+
+    public ABBInt(NoArv raiz, int quant) {
+        this.raiz = null;
+        this.quant = 0;
+    }
+
+}
